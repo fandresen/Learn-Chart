11c800c1bc289b6ed3f203cff1936dd198366fc6
fandresena <fandresenaantokiniaina1@gmail.com>
2024-05-10 13:47:09 +0200

    presentation

--- index.js
+++ index.js
@@ -1,74 +1,173 @@
-const { Chart } = require("chart.js");
 
-const canvas = document.getElementById('firstCanvas');
+const canvas = document.getElementById('firstCanvas').getContext('2d');
 
+
+
+
+//BAR
 new Chart(canvas, {
-    type: 'line',
-    options: {
-      plugins: {
-          subtitle: {
-              display: true,
-              text: 'Custom Chart Subtitle'
-          },
-          title: {
-            display: true,
-            text: 'Custom Chart Title'
-        }
-      }
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 10, 3, 5, 2, 13],
+      backgroundColor:['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      borderWidth: 1
+    }]
   },
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        backgroundColor: '#F584',
-      
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
+  options: {
+    animation:false,
     }
-  });
+});
 
-  Chart.defaults.backgroundColor = '#333';
-  Chart.defaults.borderColor = '#36A2EB'; 
-  Chart.defaults.color = '#333';    datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 4,
-        backgroundColor: '#F584',
-      
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
+
+
+//LINE
+
+/* new Chart(canvas, {
+  type: 'line',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      backgroundColor:['rgba(200,30,100,0.20)'],
+      data: [12, 10, 3, 5, 2, 13],
+      borderWidth: 1,
+      fill:true,
+      lineTension:0.3,
+      pointRadius:0
+
+     
+    }]
+  },
+  options: {}
+}); */
+
+
+
+//PIE
+/* new Chart(canvas, {
+  type: 'pie',
+  data: {
+    labels: ['Red', 'Green', 'Blue', 'Yellow', 'Purple'],
+  datasets: [{
+    label: 'My Pie Chart',
+    data: [30, 25, 40, 15, 10],
+    backgroundColor: [
+      'rgb(255, 99, 132)', // Red
+      'rgb(75, 192, 192)', // Green
+      'rgb(28, 138, 214)', // Blue
+      'rgb(255, 214, 0)', // Yellow
+      'rgb(153, 102, 200)'  // Purple
+    ],
+  }]
+  },
+  options: { responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Pie Chart less DADA ahh',
       }
-    }
-  });
-
-  Chart.defaults.backgroundColor = '#333';
-  Chart.defaults.borderColor = '#36A2EB'; 
-  Chart.defaults.color = '#333';
+    }}
+}); */
 
 
 
-// const img = new Image();
-// img.src = './sary.jpg';
-// img.onload = () => {
-//   const ctx = document.getElementById('canvas').getContext('2d');
-//   const fillPattern = ctx.createPattern(img, 'repeat');
 
-//   const chart = new Chart(canvas, {
-//     data: {
-//       labels: ['Item 1', 'Item 2', 'Item 3'],
-//       datasets: [{
-//         data: [10, 20, 30],
-//         backgroundColor: fillPattern
-//       }]
-//     }
-//   });
-// };
+//Doughnut
+/* new Chart(canvas, {
+  type: 'doughnut',
+  data: {
+    labels: ['Red', 'Green', 'Blue', 'Yellow', 'Purple'],
+  datasets: [{
+    label: 'My Pie Chart',
+    data: [30, 25, 40, 15, 10],
+    backgroundColor: [
+      'rgb(255, 99, 132)', // Red
+      'rgb(75, 192, 192)', // Green
+      'rgb(28, 138, 214)', // Blue
+      'rgb(255, 214, 0)', // Yellow
+      'rgb(153, 102, 200)'  // Purple
+    ],
+  }]
+  },
+  options: { responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Pie Chart Example'
+      }
+    }}
+}); */
+
+
+
+//RADAR
+/* new Chart(canvas, {
+  type: 'radar',
+  data: {
+    labels: ['Red', 'Green', 'Blue', 'Yellow', 'Purple'],
+  datasets: [{
+    label: 'My Pie Chart',
+    data: [30, 25, 40, 15, 10],
+    backgroundColor: [
+      'rgb(255, 99, 132)', // Red
+      'rgb(75, 192, 192)', // Green
+      'rgb(28, 138, 214)', // Blue
+      'rgb(255, 214, 0)', // Yellow
+      'rgb(153, 102, 200)'  // Purple
+    ],
+  }]
+  },
+  options: { responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Pie Chart Example'
+      }
+    }}
+}); */
+
+
+// new Chart(canvas, {
+//   type: 'polarArea',
+//   data: {
+//     labels: ['Red', 'Green', 'Blue', 'Yellow', 'Purple'],
+//   datasets: [{
+//     label: 'My Pie Chart',
+//     data: [30, 25, 40, 15, 10],
+//     backgroundColor: [
+//       'rgb(255, 99, 132)', // Red
+//       'rgb(75, 192, 192)', // Green
+//       'rgb(28, 138, 214)', // Blue
+//       'rgb(255, 214, 0)', // Yellow
+//       'rgb(153, 102, 200)'  // Purple
+//     ],
+//   }]
+//   },
+//   options: { responsive: true,
+//     plugins: {
+//       legend: {
+//         position: 'top',
+//       },
+//       title: {
+//         display: true,
+//         text: 'Pie Chart Example'
+//       }
+//     }}
+// });
+
+
+
+
+ 
